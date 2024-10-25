20843557325db0cdb4c1013b3783408ee1f6fd7d
Adit Ghosh <blubrdpirts@gmail.com>
2024-10-25 15:38:02 +0530

    Comments

--- Python.py
+++ Python.py
@@ -6,7 +6,9 @@ a = 75
 print(a)
 
 # Id functions --> gives the memory location of the variable
-
 var1 = "Adit Ghosh"
 print(id(var1)) # Answer 1 (var1 = 34) --> 140710766243288
                 # Answer 2 (var1 = "Adit Ghosh") --> 1692649809264
+
+# Comments
+# This is how you comment.
