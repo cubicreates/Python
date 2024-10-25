d07486f5b895aa78a49d408a3a1f18afaf5519f5
Adit Ghosh <blubrdpirts@gmail.com>
2024-10-25 15:41:31 +0530

    Datatypes in python

--- Python.py
+++ Python.py
@@ -8,7 +8,25 @@ print(a)
 # Id functions --> gives the memory location of the variable
 var1 = "Adit Ghosh"
 print(id(var1)) # Answer 1 (var1 = 34) --> 140710766243288
-                # Answer 2 (var1 = "Adit Ghosh") --> 1692649809264
+                # Answer 2 (var1 = "Adit Ghosh") --> 1692649809264 and ever changing
 
 # Comments
 # This is how you comment.
+
+# Datatypes in python
+type1 = 23
+type2 = 23.3
+type3 = "Adit Ghosh"
+
+t1 = type(type1)
+t2 = type(type2)
+t3 = type(type3)
+
+print(type1)
+print(t1)
+
+print(type2)
+print(t2)
+
+print(type3)
+print(t3)
